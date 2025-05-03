f1361a47b9b09dd8956a7f90e17b597dbc283089
latnikol <kolya3257393@gmail.com>
2025-05-03 15:09:37 +0700

    Initial Commit

---
+++ mobileProject/mobileProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobileProjectApp.swift
+//  mobileProject
+//
+//  Created by Nikolay Latyshev on 03.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct mobileProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mobileProjectTests/mobileProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  mobileProjectTests.swift
+//  mobileProjectTests
+//
+//  Created by Nikolay Latyshev on 03.05.2025.
+//
+
+import Testing
+@testable import mobileProject
+
+struct mobileProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
